a0960c7deb9a39ec900c2c33e4393b073bb410cb
PrathamSingh1 <prathamjii36@gmail.com>
2025-07-09 05:27:51 +0000

    Authentication Routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,6 +12,7 @@ const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
 import connectToSocket from './controllers/socketManger.js';
+import userRoutes from './routes/users.routes.js'
 
 
 app.set("port", (process.env.PORT || 8000))
@@ -19,6 +20,8 @@ app.use(cors());
 app.use(express.json({limit: "40kb"}));
 app.use(express.urlencoded({limit: "40kb", extended: true}));
 
+
+app.use("/api/v1/users", userRoutes);
 
 const start = async () => {
     app.set("mongo_user")
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -4,7 +4,7 @@ import mongoose, { Schema } from "mongoose";
 const userSchema = new Schema(
     {
         name: {type: String, required: true},
-        username: {type: String, required: true, unique: ture},
+        username: {type: String, required: true, unique: true},
         password: {type: String, required: true},
         token: {type: String}
     }
