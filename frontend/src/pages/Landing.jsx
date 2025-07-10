3386ccabe3240918e4227d436415b28fe7a28bad
PrathamSingh1 <prathamjii36@gmail.com>
2025-07-10 07:54:43 +0000

    Creating Navbar

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Landing = () => {
   return (
-    <div>Landing</div>
+    <div className='landingPageContainer'>
+        <nav>
+            <div className='navHeader'>
+                <h2>Connect2sky</h2>
+            </div>
+            <div className='navlist'>
+                <p>Join as Guest</p>
+                <p>Register</p>
+                <div role='button'>
+                    <p>Login</p>
+                </div>
+            </div>
+        </nav>
+
+        <div className="landingMainContainer">
+            <div></div>
+            <div></div>
+        </div>
+    </div>
   )
 }
 
